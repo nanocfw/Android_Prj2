944b8d6ef725996ab5d67f1034f1e5b69eb68c47
Marciano da Rocha <nanocfw@gmail.com>
2017-11-23 01:11:03 -0200

    Ajuste tipo critério

--- app/src/main/java/com/example/luanabelusso/aps_android/entidades/enums/TipoCriterio.java
+++ app/src/main/java/com/example/luanabelusso/aps_android/entidades/enums/TipoCriterio.java
@@ -6,4 +6,40 @@ package com.example.luanabelusso.aps_android.entidades.enums;
 
 public enum TipoCriterio {
     NUMEROS_ALEATORIOS, ITEM_LISTA, AUTOMATICO, ORDEM_CRESCENTE, ORDEM_DECRESCENTE, NUMEROS_PARES, NUMEROS_IMPARES;
+
+    @Override
+    public String toString() {
+        return asString(this);
+    }
+
+    public static String asString(TipoCriterio d) {
+        switch (d) {
+            case NUMEROS_ALEATORIOS:
+                return "Números Aleatórios";
+            case ITEM_LISTA:
+                return "Item da Lista";
+            case AUTOMATICO:
+                return "Automático";
+            case ORDEM_CRESCENTE:
+                return "Ordem Crescente";
+            case ORDEM_DECRESCENTE:
+                return "Ordem Decrescente";
+            case NUMEROS_PARES:
+                return "Números Pares";
+            case NUMEROS_IMPARES:
+                return "Números Ímpares";
+            default:
+                return "Inválido";
+        }
+    }
+
+    public static String[] getAll() {
+        TipoCriterio[] c = values();
+        String[] aux = new String[c.length];
+
+        for (int i = 0; i < c.length; i++)
+            aux[i] = asString(c[i]);
+
+        return aux;
+    }
 }
