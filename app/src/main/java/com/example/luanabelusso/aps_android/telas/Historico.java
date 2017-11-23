a2b435222e61570875f7508261378c5dd7d97de2
feliphe212 <felipebrenaribeiro@gmail.com>
2017-11-23 00:36:36 -0200

    Historico
    Começo Implementação Histórico

---
+++ app/src/main/java/com/example/luanabelusso/aps_android/telas/Historico.java
@@ -0,0 +1,20 @@
+package com.example.luanabelusso.aps_android.telas;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.luanabelusso.aps_android.R;
+
+/**
+ * Created by Felipe Ribeiro on 22/11/2017.
+ */
+
+public class Historico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historico);
+    }
+}
--- app/src/main/java/com/example/luanabelusso/aps_android/telas/MainActivity.java
+++ app/src/main/java/com/example/luanabelusso/aps_android/telas/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.luanabelusso.aps_android.telas;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,6 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.app.ActionBar actionBar = getActionBar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id==R.id.historico){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void sorteioSimples(View view) {
